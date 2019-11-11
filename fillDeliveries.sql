0351a45b87c8af4f219be57a564a699a891dae10
Andrew Woods <woodsaj@cardiff.ac.uk>
2019-11-11 20:12:20 +0000

    changes to fillDel

--- fillDeliveries.sql
+++ fillDeliveries.sql
@@ -1,8 +1,8 @@
 insert into Deliveries
-values(1,2,'S2', 12-DEC-2001);
+values(1,2,'S2', '12-DEC-2001');
 insert into Deliveries
-values(2,3,'S4', 01-JAN-2002);
+values(2,3,'S4', '01-JAN-2002');
 insert into Deliveries
-values(3,1,'S4', 11-SEP-2001);
+values(3,1,'S4', '11-SEP-2001');
 insert into Deliveries
-values(4,2,'S2', 15-OCT-2001);
+values(4,2,'S2', '15-OCT-2001');
