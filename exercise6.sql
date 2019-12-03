9b151e0ad77ba427184534d44035c01e5d27cbbe
Andrew Woods <woodsaj@cardiff.ac.uk>
2019-12-03 01:43:59 +0000

    5562

--- exercise6.sql
+++ exercise6.sql
@@ -1,6 +1,5 @@
-clear screen;
-accept Report_File char prompt 'Enter a filename for Summary Report ';
-col StoreCode format A8 heading 'Store Code';
+
+col StoreCode heading 'Store Code';
 col Description format A30 heading 'Item Description';
 col Price format $999,9990.99 heading 'Price';
 col Quantity format 9999 heading 'Quantity';
@@ -13,27 +12,18 @@ set newpage 2;
 set pagesize 70;
 set linesize 150;
 set underline =;
+set RecSepChar '*';
 ttitle center 'Current Stock Value by Store' skip 2 -
 center 'Prepared by ' &reporting_officer skip 2 -
 center SYSDATE skip 2;
 btitle center format 999 SQL.PNO;
 
+break on storecode
+compute sum label Store Total of (Price * Quantity) on Storecode;
+break on report
+compute sum label Grand Total of (Price * Quantity) on Report;
 
-break on storecode skip2 on Report;
-compute sum of (Price * Quantity) on storecode;
-compute sum of (Price * Quanity) on report;
-
-
-spool &Report_File;
 
 select storecode, description, price, quantity, (Price * Quantity)
 from stocks
 order by storecode;
-
-spool off;
-
-
-clear breaks;
-clear columns;
-clear computes;
-set pause on;
