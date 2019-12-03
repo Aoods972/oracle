d81ce7923872d9b457dc4c01f0e23df28972a477
Andrew Woods <woodsaj@cardiff.ac.uk>
2019-12-03 02:03:18 +0000

    779

--- exercise6.sql
+++ exercise6.sql
@@ -1,5 +1,5 @@
 
-col StoreCode heading 'Store Code';
+col StoreCode format A14 heading 'Store Code';
 col Description format A30 heading 'Item Description';
 col Price format $999,9990.99 heading 'Price';
 col Quantity format 9999 heading 'Quantity';
@@ -10,17 +10,17 @@ set feedback off;
 set space 6;
 set newpage 2;
 set pagesize 70;
-set linesize 150;
+set linesize 70;
 set underline =;
 set RecSepChar '*';
 ttitle center 'Current Stock Value by Store' skip 2 -
 center 'Prepared by ' &reporting_officer skip 2 -
-center SYSDATE skip 2;
+center select SYSDATE from dual skip 2;
 btitle center format 999 SQL.PNO;
 
 break on storecode skip 2 on Report;
-compute sum label storetotal of (Price*Quantity) on Storecode;
-compute sum label grandtotal of (Price*Quantity) on Report;
+compute sum label Store Total of (Price*Quantity) on Storecode;
+compute sum label Grand Total of (Price*Quantity) on Report;
 
 
 select storecode, description, price, quantity, (Price * Quantity)
