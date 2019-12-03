844c49a384517d40490e4c315262beee4b162c56
Andrew Woods <woodsaj@cardiff.ac.uk>
2019-12-03 02:28:08 +0000

    done

--- exercise6.sql
+++ exercise6.sql
@@ -1,4 +1,6 @@
 
+
+define Current_Time = CURRENT_DATE;
 col StoreCode format A12 heading 'Store Code';
 col Description format A30 heading 'Item Description';
 col Price format $999,9990.99 heading 'Price';
@@ -15,7 +17,7 @@ set underline =;
 set RecSepChar '*';
 ttitle center 'Current Stock Value by Store' skip 2 -
 center 'Prepared by ' &reporting_officer skip 2 -
-center SYSDATE skip 2;
+center Current_Time skip 2;
 btitle center format 999 SQL.PNO;
 
 break on storecode skip 2 on Report;
@@ -26,3 +28,4 @@ compute sum label Grand_Total of (Price*Quantity) on Report;
 select storecode, description, price, quantity, (Price * Quantity)
 from stocks
 order by storecode;
+/*No touching this code unless date can be added*/
